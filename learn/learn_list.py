4380bf067956f065d8f2d4d3a698f732a795353b
manas_maji <manas.maji240@gmail.com>
2020-09-01 20:58:42 +0530

    learned list basic

---
+++ learn/learn_list.py
@@ -0,0 +1,15 @@
+
+array = [10, 20, 30, 40, 50, 'Manas', True, False, [1, 2, 3], 50, 50]
+
+print(array[0])
+print(array[8][1])
+
+fav_pet = 'blackdog'
+print(fav_pet[0:5:3])
+
+text = 'Python is not only a snake!'
+print(text[-3: -1])
+print(text[-5:])
+
+
+
